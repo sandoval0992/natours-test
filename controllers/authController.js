8dbe0b03940960a426fd42bab0ae9df209cd13f6
sandoval0992 <srji0992@gmail.com>
2022-01-30 10:35:46 -0600

    tust proxy feature enabled

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,20 +12,16 @@ const signToken = id => {
   });
 };
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
 
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    secure: true,
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
     httpOnly: true
   };
-
-  if (process.env.NODE_ENV === "production") {
-    cookieOptions.secure = true;
-  }
 
   res.cookie("jwt", token, cookieOptions);
 
@@ -53,7 +49,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   const url = `${req.protocol}://${req.get("host")}/me`;
   await new Email(newUser, url).sendWelcome();
 
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -78,7 +74,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   // 3) It everything ok, send token to client
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 exports.logout = (req, res) => {
@@ -240,7 +236,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   await user.save();
 
   //4 Log the user in, send JWT
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -266,5 +262,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   await user.save();
 
   // 4) Log user in, send JWT
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
