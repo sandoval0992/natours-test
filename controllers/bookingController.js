9cdbd45e88b1166052aafd28b376a9c94dba979e
sandoval0992 <srji0992@gmail.com>
2022-02-03 23:16:34 -0600

    bookingController update

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -19,7 +19,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
-    display_items: [
+    line_items: [
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
@@ -55,7 +55,7 @@ const createBookingCheckout = async session => {
   console.log(session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.line_items[0].amount / 100;
   console.log(tour, user, price);
   await Booking.create({ tour, user, price });
   console.log("Booking created successfully.");
